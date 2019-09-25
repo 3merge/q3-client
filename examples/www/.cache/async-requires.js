8bfce0a78fa274fc0b41b0f8cd455aaa5569813d
Mike <mibberson@3merge.ca>
2019-09-25 09:08:34 -0400

    Relocated example projects

---
+++ examples/www/.cache/async-requires.js
@@ -0,0 +1,14 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---gatsby-theme-q-3-src-templates-contact-jsx": () => import("C:\\Users\\ibber\\Documents\\Q3\\client\\gatsby-theme-q3\\src\\templates\\contact.jsx" /* webpackChunkName: "component---gatsby-theme-q-3-src-templates-contact-jsx" */),
+  "component---gatsby-theme-q-3-src-templates-login-jsx": () => import("C:\\Users\\ibber\\Documents\\Q3\\client\\gatsby-theme-q3\\src\\templates\\login.jsx" /* webpackChunkName: "component---gatsby-theme-q-3-src-templates-login-jsx" */),
+  "component---gatsby-theme-q-3-src-templates-password-reset-jsx": () => import("C:\\Users\\ibber\\Documents\\Q3\\client\\gatsby-theme-q3\\src\\templates\\password-reset.jsx" /* webpackChunkName: "component---gatsby-theme-q-3-src-templates-password-reset-jsx" */),
+  "component---gatsby-theme-q-3-src-templates-verify-jsx": () => import("C:\\Users\\ibber\\Documents\\Q3\\client\\gatsby-theme-q3\\src\\templates\\verify.jsx" /* webpackChunkName: "component---gatsby-theme-q-3-src-templates-verify-jsx" */),
+  "component---gatsby-theme-q-3-src-templates-reverify-jsx": () => import("C:\\Users\\ibber\\Documents\\Q3\\client\\gatsby-theme-q3\\src\\templates\\reverify.jsx" /* webpackChunkName: "component---gatsby-theme-q-3-src-templates-reverify-jsx" */),
+  "component---cache-dev-404-page-js": () => import("C:\\Users\\ibber\\Documents\\Q3\\client\\example-www\\.cache\\dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-index-jsx": () => import("C:\\Users\\ibber\\Documents\\Q3\\client\\example-www\\src\\pages\\index.jsx" /* webpackChunkName: "component---src-pages-index-jsx" */),
+  "component---src-pages-about-jsx": () => import("C:\\Users\\ibber\\Documents\\Q3\\client\\example-www\\src\\pages\\about.jsx" /* webpackChunkName: "component---src-pages-about-jsx" */)
+}
+
---
+++ examples/www/.cache/sync-requires.js
@@ -0,0 +1,17 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---gatsby-theme-q-3-src-templates-contact-jsx": hot(preferDefault(require("C:\\Users\\ibber\\Documents\\Q3\\client\\gatsby-theme-q3\\src\\templates\\contact.jsx"))),
+  "component---gatsby-theme-q-3-src-templates-login-jsx": hot(preferDefault(require("C:\\Users\\ibber\\Documents\\Q3\\client\\gatsby-theme-q3\\src\\templates\\login.jsx"))),
+  "component---gatsby-theme-q-3-src-templates-password-reset-jsx": hot(preferDefault(require("C:\\Users\\ibber\\Documents\\Q3\\client\\gatsby-theme-q3\\src\\templates\\password-reset.jsx"))),
+  "component---gatsby-theme-q-3-src-templates-verify-jsx": hot(preferDefault(require("C:\\Users\\ibber\\Documents\\Q3\\client\\gatsby-theme-q3\\src\\templates\\verify.jsx"))),
+  "component---gatsby-theme-q-3-src-templates-reverify-jsx": hot(preferDefault(require("C:\\Users\\ibber\\Documents\\Q3\\client\\gatsby-theme-q3\\src\\templates\\reverify.jsx"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\ibber\\Documents\\Q3\\client\\example-www\\.cache\\dev-404-page.js"))),
+  "component---src-pages-index-jsx": hot(preferDefault(require("C:\\Users\\ibber\\Documents\\Q3\\client\\example-www\\src\\pages\\index.jsx"))),
+  "component---src-pages-about-jsx": hot(preferDefault(require("C:\\Users\\ibber\\Documents\\Q3\\client\\example-www\\src\\pages\\about.jsx")))
+}
+
