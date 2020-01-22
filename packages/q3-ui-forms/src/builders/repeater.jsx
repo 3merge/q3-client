cd1f4a1813d92b4eb221b17cdd254919e99ebd68
asifdev <anttor062@gmail.com>
2020-01-22 11:37:58 -0500

    Update 'Folder' icon into 'Pen' Icon in Repeater

--- packages/q3-ui-forms/src/builders/repeater.jsx
+++ packages/q3-ui-forms/src/builders/repeater.jsx
@@ -8,8 +8,8 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import FolderIcon from '@material-ui/icons/Folder';
 import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
 import IconButton from '@material-ui/core/IconButton';
 import Dialog from 'q3-ui-dialog';
 import * as yup from 'yup';
@@ -103,7 +103,7 @@ export const DataList = ({
             renderContent={getForm(false, item, item.id)}
             renderTrigger={(open) => (
               <IconButton onClick={open}>
-                <FolderIcon />
+                <EditIcon />
               </IconButton>
             )}
           />
