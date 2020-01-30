fc5a076adc6fcc35e0a305ed8fff2d0e8218f7da
Mike <mibberson@3merge.ca>
2020-01-30 11:47:14 -0500

    test: filter container submit/reset action handlers

---
+++ packages/q3-admin/src/containers/filter/__tests__/submitActions.unit.test.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import SplitButton from 'q3-ui/lib/splitButton';
+import { SubmitActions } from '../submitActions';
+
+const getProps = () => ({
+  formik: {
+    status: 'init',
+    submitForm: jest.fn(),
+    isSubmitting: false,
+    values: {
+      garply: true,
+      foo: {
+        bar: {
+          baz: true,
+        },
+      },
+      quuz: [1, 2],
+    },
+  },
+  params: {
+    delete: jest.fn(),
+  },
+});
+
+const callHandlerByIndex = (props, i) =>
+  global
+    .shallow(<SubmitActions {...props} />)
+    .find(SplitButton)
+    .props()
+    .options[i].handler();
+
+describe('"Filter/SubmitActions"', () => {
+  it('should call formik submitForm', () => {
+    const props = getProps();
+    callHandlerByIndex(props, 0);
+    expect(props.formik.submitForm).toHaveBeenCalled();
+  });
+
+  it('should call delete on all value keys', () => {
+    const props = getProps();
+    callHandlerByIndex(props, 1);
+    expect(props.params.delete).toHaveBeenCalledWith(
+      'garply',
+    );
+
+    expect(props.params.delete).toHaveBeenCalledWith(
+      'quuz',
+    );
+
+    expect(props.params.delete).toHaveBeenCalledWith(
+      'foo.bar.baz',
+    );
+  });
+});
--- packages/q3-admin/src/containers/filter/submitActions.jsx
+++ packages/q3-admin/src/containers/filter/submitActions.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import flat from 'flat';
 import { useTranslation } from 'react-i18next';
 import { connect } from 'formik';
 import { withLocation } from 'with-location';
@@ -6,48 +8,59 @@ import Box from '@material-ui/core/Box';
 import SplitButton from 'q3-ui/lib/splitButton';
 import { goTo } from './utils';
 
-export default withLocation(
-  connect(
-    ({
-      formik: { status, submitForm, isSubmitting, values },
-      params,
-    }) => {
-      const { t } = useTranslation();
+export const SubmitActions = ({
+  formik: { status, submitForm, isSubmitting, values },
+  params,
+}) => {
+  const { t } = useTranslation();
 
-      return (
-        <Box mt={1}>
-          <SplitButton
-            size="normal"
-            color="secondary"
-            loading={isSubmitting}
-            options={[
-              {
-                label: t('labels:apply'),
-                description: t('labels:applyAndSave'),
-                handler: submitForm,
-              },
+  return (
+    <Box mt={1}>
+      <SplitButton
+        size="normal"
+        color="secondary"
+        loading={isSubmitting}
+        options={[
+          {
+            label: t('labels:apply'),
+            description: t('labels:applyAndSave'),
+            handler: submitForm,
+          },
 
-              {
-                label: t('labels:reset'),
-                description: t('descriptions:reset'),
-                handler: () => {
-                  Object.keys(values).forEach((v) =>
-                    params.delete(v),
-                  );
+          {
+            label: t('labels:reset'),
+            description: t('descriptions:reset'),
+            handler: () => {
+              Object.keys(
+                flat(values, { safe: true }),
+              ).forEach((v) => params.delete(v));
 
-                  return goTo(status, params);
-                },
-              },
-            ]}
-          />
-        </Box>
-      );
-    },
-  ),
-);
-/*
-              {
-                label: t('labels:applyAndSave'),
-                description: t('descriptions:applyAndSave'),
-                handler: submitForm,
-              }, */
+              return goTo(status, params);
+            },
+          },
+        ]}
+      />
+    </Box>
+  );
+};
+
+SubmitActions.propTypes = {
+  /**
+   * Injected by formik
+   */
+  formik: PropTypes.shape({
+    status: PropTypes.string,
+    submitForm: PropTypes.func,
+    isSubmitting: PropTypes.bool,
+    values: PropTypes.object,
+  }).isRequired,
+
+  /**
+   * Injected by with-location
+   */
+  params: PropTypes.shape({
+    delete: PropTypes.func,
+  }).isRequired,
+};
+
+export default withLocation(connect(SubmitActions));
