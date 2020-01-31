8b8accdb34c714c82a9534e9ed14167b1cc03215
Mike Ibberson <32675502+MikeIbberson@users.noreply.github.com>
2020-01-30 21:19:44 -0500

    Dynamic header title rendering (#88)
    * tests: header with custom title assembly
    
    * docs: header prop table

---
+++ packages/q3-admin/src/containers/header/header.stories.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import State from '../state';
+import Header from '.';
+
+export default {
+  title: 'Containers/Header',
+  parameters: {
+    component: Header,
+    componentSubtitle:
+      'Admin app bar with dynamic title rendering',
+  },
+};
+
+const withProvider = (Component) => (props) => (
+  <State.Provider
+    value={{
+      resourceName: 'resources',
+      resourceNameSingular: 'resource',
+      id: 1,
+      resource: {
+        name: 'Jon',
+        email: 'jon@gmail.ca',
+      },
+    }}
+  >
+    <Component {...props} />
+  </State.Provider>
+);
+
+export const Default = withProvider(() => <Header />);
+
+export const WithChildren = withProvider(() => (
+  <Header>
+    <p>Render</p>
+  </Header>
+));
+
+export const WithCustomTitle = withProvider(() => (
+  <Header titleProp="name" />
+));
+
+export const WithCustomTitleAndSubtitle = withProvider(
+  () => <Header titleProp="name" subtitleProp="email" />,
+);
