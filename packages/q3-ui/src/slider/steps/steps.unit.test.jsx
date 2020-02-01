aa0e46cf42d9edaca6fc4e2c843c7d8cf1c96c56
Mike <mibberson@3merge.ca>
2020-02-01 13:07:28 -0500

    tests: slider step rendering

---
+++ packages/q3-ui/src/slider/steps/steps.unit.test.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Swiper from 'react-id-swiper';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Steps from '.';
+
+jest.mock('../useStyle', () => () => ({
+  thumbs: null,
+}));
+
+const getProps = (args) => ({
+  current: 0,
+  swiper: {
+    controller: {
+      control: undefined,
+    },
+  },
+  ...args,
+});
+
+describe('Slider/Steps', () => {
+  it('should render Swiper component', () =>
+    expect(
+      global
+        .shallow(
+          <Steps {...getProps({ withThumbnails: true })} />,
+        )
+        .find(Swiper),
+    ).toHaveLength(1));
+
+  it('should render MobileStepper component', () =>
+    expect(
+      global
+        .shallow(<Steps {...getProps()} />)
+        .find(MobileStepper),
+    ).toHaveLength(1));
+});
