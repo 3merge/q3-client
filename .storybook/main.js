2f168f4518f17dfe24edc88eb90388909ae48ce7
Mike Ibberson <32675502+MikeIbberson@users.noreply.github.com>
2020-02-10 08:27:18 -0500

    [FIX] Search typography components (#99)
    * fix: Q3-47 breadcrumb locale
    
    * fix: Q3-77 renderer prop func
    
    * fix: prop drilling companyName into ProfileBar Q3-80
    
    * fix: remove filter title Q3-83
    
    * fix: uri encoding feat for q3-ui-filters
    
    * fix: scrollable prop on group (Q3-82)
    
    * fix: disable link ability on data row (Q3-81)
    
    * fix: add search text to panel (Q3-85)
    
    * fix: searchbar text

--- .storybook/main.js
+++ .storybook/main.js
@@ -11,6 +11,7 @@ module.exports = {
     '../packages/q3-ui/**/breadcrumbs/**.stories.jsx',
     '../packages/q3-ui/**/card/*.stories.jsx',
     '../packages/q3-ui/**/list/*.stories.jsx',
+    '../packages/q3-ui/**/searchBar/**.stories.jsx',
     '../packages/q3-ui/**/slider/**.stories.jsx',
     '../packages/q3-ui/**/splitButton/*.stories.jsx',
     '../packages/q3-ui-forms/**/builders/form.stories.jsx',
