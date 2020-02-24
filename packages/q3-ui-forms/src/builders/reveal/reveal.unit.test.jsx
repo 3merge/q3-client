0345c97b32300944fa2ca92ec9674b92526607cb
Mike <mibberson@3merge.ca>
2020-02-24 17:13:30 -0500

    refactor: reduced redundancy in reveal test

--- packages/q3-ui-forms/src/builders/reveal/reveal.unit.test.jsx
+++ packages/q3-ui-forms/src/builders/reveal/reveal.unit.test.jsx
@@ -2,30 +2,24 @@ import React from 'react';
 import Fade from '@material-ui/core/Fade';
 import Reveal from '.';
 
-describe('Reveal', () => {
-  it('should fade in', () => {
-    const f = global
+const checkFadeProps = (_nodes = {}, inValue) =>
+  expect(
+    global
       .shallow(
-        <Reveal validation={{ _nodes: {} }}>
+        <Reveal validation={{ _nodes }}>
           {jest.fn()}
         </Reveal>,
       )
       .find(Fade)
-      .props();
+      .props(),
+  ).toHaveProperty('in', inValue);
 
-    expect(f).toHaveProperty('in', false);
+describe('Reveal', () => {
+  it('should fade in', () => {
+    checkFadeProps({}, false);
   });
 
   it('should fade out', () => {
-    const f = global
-      .shallow(
-        <Reveal validation={{ _nodes: { length: 2 } }}>
-          {jest.fn()}
-        </Reveal>,
-      )
-      .find(Fade)
-      .props();
-
-    expect(f).toHaveProperty('in', true);
+    checkFadeProps({ length: 2 }, true);
   });
 });
