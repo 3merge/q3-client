989aff8060ebda7a1d02d682d3ffbf877c8e76f6
Mike <mibberson@3merge.ca>
2020-03-04 11:03:49 -0500

    docs: registered all form builders in storybook

--- packages/q3-ui-forms/src/builders/repeater.stories.jsx
+++ packages/q3-ui-forms/src/builders/repeater.stories.jsx
@@ -1,107 +1,116 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
 import Tile from 'q3-ui/lib/tile';
 import Multistep, { Fieldset } from './multistep';
 import Repeater from './repeater';
 import Form from './form';
 import Field from './field';
 
-storiesOf('Forms|Repeater', module)
-  .add('Without data', () => (
-    <Tile
-      title="Characters"
-      subtitle="This is an important blurb about the characters"
-    >
-      <Repeater name="Main Characters">
-        <div>Requires a child</div>
-      </Repeater>
-    </Tile>
-  ))
-  .add('With simple form', () => (
-    <Tile
-      title="Characters"
-      subtitle="This is an important blurb about the characters"
-    >
-      <Repeater
-        data={[
-          { id: 1, firstName: 'Joe', lastName: 'Snow' },
-          { id: 2, firstName: 'Arya', lastName: 'Stark' },
-        ]}
-        title="lorem"
-        create={() => Promise.resolve()}
-        edit={() => () => Promise.resolve()}
-        primary={({ firstName }) =>
-          `First Name: ${firstName}`
-        }
-        secondary={({ lastName }) =>
-          `Family Name: ${lastName}`
-        }
-        initialValues={{
-          firstName: '',
-        }}
-      >
-        <Form label="add">
-          <Field type="text" name="firstName" required />
-        </Form>
-      </Repeater>
-    </Tile>
-  ))
-  .add('With wizard', () => (
-    <Tile
-      title="Characters"
-      subtitle="This is an important blurb about the characters"
-    >
-      <Repeater
-        remove={() => null}
-        create={() => null}
-        title="lorem"
-        data={[
-          {
-            id: 1,
-            firstName: 'Joe',
-            lastName: 'Snow',
-            age: 11,
-            company: '3merge',
-          },
-          {
-            id: 2,
-            firstName: 'Arya',
-            lastName: 'Stark',
-            age: 23,
-            position: 'Sales',
-          },
-        ]}
-        primary={({ firstName }) =>
-          `First Name: ${firstName}`
-        }
-        secondary={({ lastName }) =>
-          `Family Name: ${lastName}`
-        }
-        initialValues={{
-          firstName: '',
-          lastName: '',
-        }}
-        edit={() => (values, actions) => {
-          actions.setFieldError(
-            'firstName',
-            'We do not know you',
-            false,
-          );
+export default {
+  title: 'Q3 Forms|Builders/Repeaters',
+  parameters: {
+    component: Repeater,
+    componentSubtitle:
+      'Configure forms for repeatable embedded documents',
+  },
+};
 
-          return Promise.reject();
-        }}
-      >
-        <Multistep>
-          <Fieldset name="General">
-            <Field type="text" name="firstName" required />
-            <Field type="text" name="lastName" required />
-            <Field type="number" name="age" min="21" />
-          </Fieldset>
-          <Fieldset name="Work">
-            <Field type="text" name="position" />
-            <Field type="text" name="company" />
-          </Fieldset>
-        </Multistep>
-      </Repeater>
-    </Tile>
-  ));
+export const Empty = () => (
+  <Tile
+    title="Characters"
+    subtitle="This is an important blurb about the characters"
+  >
+    <Repeater name="Main Characters">
+      <div>Requires a child</div>
+    </Repeater>
+  </Tile>
+);
+
+export const WithForm = () => (
+  <Tile
+    title="Characters"
+    subtitle="This is an important blurb about the characters"
+  >
+    <Repeater
+      data={[
+        { id: 1, firstName: 'Joe', lastName: 'Snow' },
+        { id: 2, firstName: 'Arya', lastName: 'Stark' },
+      ]}
+      title="lorem"
+      create={() => Promise.resolve()}
+      edit={() => () => Promise.resolve()}
+      primary={({ firstName }) =>
+        `First Name: ${firstName}`
+      }
+      secondary={({ lastName }) =>
+        `Family Name: ${lastName}`
+      }
+      initialValues={{
+        firstName: '',
+      }}
+    >
+      <Form label="add">
+        <Field type="text" name="firstName" required />
+      </Form>
+    </Repeater>
+  </Tile>
+);
+
+export const WithWizard = () => (
+  <Tile
+    title="Characters"
+    subtitle="This is an important blurb about the characters"
+  >
+    <Repeater
+      remove={() => null}
+      create={() => null}
+      title="lorem"
+      data={[
+        {
+          id: 1,
+          firstName: 'Joe',
+          lastName: 'Snow',
+          age: 11,
+          company: '3merge',
+        },
+        {
+          id: 2,
+          firstName: 'Arya',
+          lastName: 'Stark',
+          age: 23,
+          position: 'Sales',
+        },
+      ]}
+      primary={({ firstName }) =>
+        `First Name: ${firstName}`
+      }
+      secondary={({ lastName }) =>
+        `Family Name: ${lastName}`
+      }
+      initialValues={{
+        firstName: '',
+        lastName: '',
+      }}
+      edit={() => (values, actions) => {
+        actions.setFieldError(
+          'firstName',
+          'We do not know you',
+          false,
+        );
+
+        return Promise.reject();
+      }}
+    >
+      <Multistep>
+        <Fieldset name="General">
+          <Field type="text" name="firstName" required />
+          <Field type="text" name="lastName" required />
+          <Field type="number" name="age" min="21" />
+        </Fieldset>
+        <Fieldset name="Work">
+          <Field type="text" name="position" />
+          <Field type="text" name="company" />
+        </Fieldset>
+      </Multistep>
+    </Repeater>
+  </Tile>
+);
