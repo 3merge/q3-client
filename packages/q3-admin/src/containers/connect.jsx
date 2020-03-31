156135ef4c8ef878fda5f577e20521e3b6ac315f
Mike <mibberson@3merge.ca>
2020-03-31 17:39:30 -0400

    fix: connect empty state checking

--- packages/q3-admin/src/containers/connect.jsx
+++ packages/q3-admin/src/containers/connect.jsx
@@ -2,17 +2,16 @@ import React from 'react';
 import Tile from 'q3-ui/lib/tile';
 import PersistWatcher from 'q3-ui-forms/lib/builders/persistWatcher';
 import { get } from 'lodash';
-import Comparision from 'comparisons';
+import { object } from 'q3-ui-helpers';
 import { Definitions, Dispatcher, Store } from './state';
 
-export default (Component, opts = {}) => ({ name }) => {
+export default (Component) => ({ name }) => {
   const { data } = React.useContext(Store);
   const { remove, patch } = React.useContext(Dispatcher);
   const { id, collectionName } = React.useContext(
     Definitions,
   );
 
-  const { conditionals = [] } = opts;
   const sessionKey = `${name}-${id}`;
 
   const createdBy = get(
@@ -21,8 +20,7 @@ export default (Component, opts = {}) => ({ name }) => {
     get(data, 'createdBy', null),
   );
 
-  if (!new Comparision(conditionals).eval(data))
-    return null;
+  if (!object.hasKeys(data)) return null;
 
   return (
     <Tile title={name} subtitle={name}>
