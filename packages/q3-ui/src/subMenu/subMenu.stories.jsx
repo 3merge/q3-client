f9572c1b1c328954c6c6e004456739a1cdca439d
Mike <mibberson@3merge.ca>
2020-04-06 23:44:34 -0400

    fix: close submenu on click

--- packages/q3-ui/src/subMenu/subMenu.stories.jsx
+++ packages/q3-ui/src/subMenu/subMenu.stories.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from '@reach/router';
+import Location from 'q3-ui-test-utils/lib/location';
 import SubMenu, { SubMenuItem } from './subMenu';
 
 export default {
@@ -10,18 +12,20 @@ export default {
 };
 
 export const Demo = () => (
-  <SubMenu>
-    <SubMenuItem
-      label="Dropdown"
-      renderer={() => 'Interior!'}
-    />
-    <SubMenuItem
-      label="Dropdown2"
-      renderer={() => 'Interior 2!'}
-    />
-    <SubMenuItem
-      label="Dropdown3"
-      renderer={() => 'Interior 3!'}
-    />
-  </SubMenu>
+  <Location initialPath="/">
+    <SubMenu>
+      <SubMenuItem
+        label="Dropdown"
+        renderer={() => <Link to="sub">Sub</Link>}
+      />
+      <SubMenuItem
+        label="Dropdown2"
+        renderer={() => <Link to="sub1">Sub1</Link>}
+      />
+      <SubMenuItem
+        label="Dropdown3"
+        renderer={() => <Link to="sub2">Sub2</Link>}
+      />
+    </SubMenu>
+  </Location>
 );
