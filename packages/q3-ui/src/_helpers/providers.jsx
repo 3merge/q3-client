66995959f1f89ca1aca622eeca6bc8e50b0395a2
Mike <mibberson@3merge.ca>
2020-04-11 12:36:19 -0400

    chore(providers): provide ability to merge themes directly in provider

--- packages/q3-ui/src/_helpers/providers.jsx
+++ packages/q3-ui/src/_helpers/providers.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import Helmet from 'react-helmet';
+import { merge } from 'lodash';
+import { Helmet } from 'react-helmet';
 import PropTypes from 'prop-types';
 import MomentUtils from '@date-io/moment';
 import Locale from 'q3-ui-locale';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import baseQ3Theme from '../mui';
 
-import theme from '../mui';
-
-const Providers = ({ children }) => (
+const Providers = ({ children, theme }) => (
   <Locale>
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={merge(baseQ3Theme, theme)}>
       <CssBaseline />
       <Helmet>
         <link
@@ -28,6 +28,13 @@ const Providers = ({ children }) => (
 
 Providers.propTypes = {
   children: PropTypes.node.isRequired,
+  theme: PropTypes.shape({
+    palette: PropTypes.object,
+  }),
+};
+
+Providers.defaultProps = {
+  theme: {},
 };
 
 export default Providers;
