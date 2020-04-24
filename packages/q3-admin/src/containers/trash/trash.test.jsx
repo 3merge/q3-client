b1b25d8d292c3c5a5aed13f506f080200d14ddeb
Mike <mibberson@3merge.ca>
2020-04-24 08:50:45 -0400

    test(j3-119): ensure that navigate does not execute on service fail

--- packages/q3-admin/src/containers/trash/trash.test.jsx
+++ packages/q3-admin/src/containers/trash/trash.test.jsx
@@ -1,52 +1,93 @@
 import React from 'react';
 import { navigate } from '@reach/router';
 import Button from '@material-ui/core/Button';
-import Trash from '.';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Alert from 'q3-ui/lib/alert';
+import { Trash } from '.';
 
+jest.useFakeTimers();
 jest.mock('../state');
 
 jest.mock('@reach/router', () => ({
   navigate: jest.fn(),
 }));
 
+const makeThenable = () => {
+  const promise = {};
+
+  const thenable = (fn) => {
+    fn();
+    return promise;
+  };
+
+  return [promise, thenable];
+};
+
 beforeEach(() => {
   navigate.mockReset();
-  jest.spyOn(React, 'useContext').mockReturnValue({
-    data: {},
-    patch: jest.fn().mockReturnValue(jest.fn()),
-    remove: jest.fn().mockReturnValue(jest.fn()),
-    id: 1,
-  });
 });
 
 describe('Trash', () => {
-  const renderAndClick = () => {
+  const renderAndClick = (onDelete) => {
     return global
-      .shallow(<Trash />)
+      .shallow(
+        <Trash
+          onDelete={onDelete}
+          directoryPath="/app/foo"
+          collectionName="foo"
+        />,
+      )
       .find(Button)
       .props()
       .onClick();
   };
 
-  it.skip('should redirect on resolve', (done) => {
-    const onClick = jest.fn().mockResolvedValue();
-    renderAndClick(onClick);
-
-    setTimeout(() => {
-      expect(onClick).toHaveBeenCalled();
-      expect(navigate).toHaveBeenCalledWith('/');
-      done();
+  it('should redirect on resolve', () => {
+    const onClick = jest.fn().mockImplementation(() => {
+      const [promise, thenable] = makeThenable();
+      promise.then = thenable;
+      promise.catch = thenable;
+      promise.finally = thenable;
+      return promise;
     });
+
+    renderAndClick(onClick);
+    jest.runAllTimers();
+
+    expect(onClick).toHaveBeenCalled();
+    expect(navigate).toHaveBeenCalledWith('/app/foo');
   });
 
-  it.skip('should do nothing on reject', (done) => {
-    const onClick = jest.fn().mockRejectedValue();
-    renderAndClick(onClick);
-
-    setTimeout(() => {
-      expect(onClick).toHaveBeenCalled();
-      expect(navigate).not.toHaveBeenCalledWith('/');
-      done();
+  it('should do nothing on reject', () => {
+    const onClick = jest.fn().mockImplementation(() => {
+      const [promise, thenable] = makeThenable();
+      promise.then = () => promise;
+      promise.catch = thenable;
+      promise.finally = thenable;
+      return promise;
     });
+
+    renderAndClick(onClick);
+    jest.runAllTimers();
+
+    expect(onClick).toHaveBeenCalled();
+    expect(navigate).not.toHaveBeenCalledWith('/app/foo');
+  });
+
+  it('should render alerts', () => {
+    jest
+      .spyOn(React, 'useState')
+      .mockImplementation(() => [true, jest.fn()]);
+
+    const el = global.shallow(
+      <Trash
+        onDelete={jest.fn()}
+        directoryPath="/app/foo"
+        collectionName="foo"
+      />,
+    );
+
+    expect(el.find(Alert)).toHaveLength(2);
+    expect(el.find(CircularProgress)).toHaveLength(1);
   });
 });
