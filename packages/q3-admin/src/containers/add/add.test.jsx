b2a374c3e34641367c1d1c4a6908970351844967
Mike Ibberson <32675502+MikeIbberson@users.noreply.github.com>
2020-04-24 09:43:57 -0400

    [Enhancement] Redirect on create (#157)
    * chore(j3-121): forward all props into the preset component
    
    * test(j3-115): ensure that navigate is called AFTER custom callback
    
    * chore(storybook): update the add component

--- packages/q3-admin/src/containers/add/add.test.jsx
+++ packages/q3-admin/src/containers/add/add.test.jsx
@@ -1,15 +1,67 @@
 import React from 'react';
-import Add from './add';
+import { navigate } from '@reach/router';
+import Add, {
+  addToDirectoryPath,
+  getIdByKey,
+  handleSuccess,
+} from './add';
 import CreateDialog from './dialog';
 
-jest.spyOn(React, 'useContext').mockReturnValue({
-  collectionName: 'demo',
-  post: jest.fn(),
-});
+jest.mock('q3-ui-permissions', () => ({
+  useAuth: jest.fn().mockReturnValue({
+    Hide: jest
+      .fn()
+      .mockImplementation(({ children }) => children),
+  }),
+}));
+
+jest.mock('@reach/router', () => ({
+  navigate: jest.fn(),
+}));
 
 describe('Add', () => {
   it('should block Dialog from rendering without children', () => {
-    const el = global.shallow(<Add title="foo" />);
+    const el = global.shallow(<Add />);
     expect(el.find(CreateDialog)).toHaveLength(0);
+  });
+
+  it('should return the ID', () => {
+    expect(getIdByKey({ foo: { id: 1 } }, 'foo')).toBe(1);
+  });
+
+  it('should not call navigate without an ID', () => {
+    addToDirectoryPath('/app/foo');
+    expect(navigate).not.toHaveBeenCalled();
+  });
+
+  it('should call navigate with an ID', () => {
+    addToDirectoryPath('/app/foo', 1);
+    expect(navigate).toHaveBeenCalledWith('/app/foo/1');
+  });
+
+  it('should call next', () => {
+    const fn = jest.fn();
+    handleSuccess('foo', 'bar', fn)({ done: 1 });
+    expect(fn).toHaveBeenCalledWith({ done: 1 });
+  });
+
+  it('should pass properties to children', () => {
+    const spy = jest.spyOn(React, 'cloneElement');
+
+    global
+      .shallow(
+        <Add onComplete={jest.fn()}>
+          <div />
+        </Add>,
+      )
+      .find(CreateDialog)
+      .props()
+      .children();
+
+    expect(spy).toHaveBeenCalledWith(expect.any(Object), {
+      isNew: true,
+      collectionName: null,
+      onSubmit: expect.any(Function),
+    });
   });
 });
