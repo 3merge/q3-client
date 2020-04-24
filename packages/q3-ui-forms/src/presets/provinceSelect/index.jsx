bcce276026008196e13e763bc37e4ec8331bc0f1
Mike Ibberson <32675502+MikeIbberson@users.noreply.github.com>
2020-04-24 09:25:32 -0400

    [Chore] Prop-forwarding in <ProvinceSelect />

--- packages/q3-ui-forms/src/presets/provinceSelect/index.jsx
+++ packages/q3-ui-forms/src/presets/provinceSelect/index.jsx
@@ -4,12 +4,13 @@ import { Field } from '../../builders';
 import { asOptions } from '../../helpers';
 import { PROVINCES } from '../northAmericaRegionalSelect';
 
-const ProvinceSelect = ({ name, required }) => (
+const ProvinceSelect = ({ name, required, ...rest }) => (
   <Field
     name={name}
     type="select"
     options={asOptions(PROVINCES)}
     required={required}
+    {...rest}
   />
 );
 
