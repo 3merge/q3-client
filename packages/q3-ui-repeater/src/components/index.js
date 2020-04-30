039bad939cba085f292f2b4dd341358d05683db7
Mike <mibberson@3merge.ca>
2020-04-30 13:49:41 -0400

    chore(editableTypography): named export

--- packages/q3-ui-repeater/src/components/index.js
+++ packages/q3-ui-repeater/src/components/index.js
@@ -1,6 +1,7 @@
 export { default as Auth } from './Auth';
 export { default as AddButton } from './AddButton';
 export { default as BulkEditorDrawer } from './BulkEditorDrawer';
+export { default as EditableTypography } from './EditableTypography';
 export { default as DeleteModal } from './DeleteModal';
 export { default as List } from './List';
 export { default as Search } from './Search';
