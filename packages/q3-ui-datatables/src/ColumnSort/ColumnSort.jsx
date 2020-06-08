9c66755794555c66c55a4416eeb957b2df2ba3ee
Mike <mibberson@3merge.ca>
2020-06-08 11:52:14 -0400

    chore: removed unused deps causing issues with core-js

--- packages/q3-ui-datatables/src/ColumnSort/ColumnSort.jsx
+++ packages/q3-ui-datatables/src/ColumnSort/ColumnSort.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { useTranslation } from 'react-i18next';
 import TableCell from '@material-ui/core/TableCell';
-import Sticky from 'react-stickynode';
 import withSort from '../withSort';
 import useStyles from '../utils/useStyles';
 
