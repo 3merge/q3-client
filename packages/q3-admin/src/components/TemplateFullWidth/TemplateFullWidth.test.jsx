7c9f29dc66b71a75584633f6116cf563435845a0
Mike <mibberson@3merge.ca>
2020-08-27 12:48:36 -0400

    test(wip): to-dos for template component

---
+++ packages/q3-admin/src/components/TemplateFullWidth/TemplateFullWidth.test.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import TemplateFullWidth from './TemplateFullWidth';
+
+describe('TemplateFullWidth', () => {
+  it.todo('should render aside when passed a component');
+  it.todo('should not render aside without a component');
+});
