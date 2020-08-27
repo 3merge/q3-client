f70b465c71ada4669f4cab2542d758959909757d
Takuya Hirata <takuyahirata4@gmail.com>
2020-08-27 14:38:07 -0400

    test: ProfileNavigation

--- packages/q3-admin/src/containers/ProfileNavigation/ProfileNavigation.test.jsx
+++ packages/q3-admin/src/containers/ProfileNavigation/ProfileNavigation.test.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import ProfileNavigation from './ProfileNavigation';
+import TemplateFullWidth from '../../components/TemplateFullWidth';
+
+const context = jest
+  .spyOn(React, 'useContext')
+  .mockReturnValue({
+    state: { profile: { photo: 'https://image.ca' } },
+    update: jest.fn(),
+  });
+
+const renderProfileNavigation = (withPhoto) =>
+  global.shallow(
+    <ProfileNavigation withPhoto={withPhoto} />,
+  );
 
 describe('ProfileNavigation', () => {
-  it.todo(
-    'should render FeaturedPhoto component  when withPhoto is true',
-  );
-  it.todo(
-    'should not render FeaturedPhoto component when withPhoto is false',
-  );
+  it('should render FeaturedPhoto component  when withPhoto is true', () => {
+    const wrapper = renderProfileNavigation(true);
+    expect(
+      wrapper.find(TemplateFullWidth).props()
+        .asideComponent,
+    ).toBeDefined();
+  });
+
+  it('should not render FeaturedPhoto component when withPhoto is false', () => {
+    const wrapper = renderProfileNavigation(false);
+    expect(
+      wrapper.find(TemplateFullWidth).props()
+        .asideComponent,
+    ).toBeNull();
+  });
 });
