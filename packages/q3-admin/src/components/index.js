a3cc11318be25f6d348cb5ba8d7c4154612f2e23
Mike <mibberson@3merge.ca>
2020-08-31 09:43:43 -0400

    style: side panel content component to standardize sidebar actions

--- packages/q3-admin/src/components/index.js
+++ packages/q3-admin/src/components/index.js
@@ -3,6 +3,7 @@ export { default as Loader } from './loader';
 export { default as Main } from './main';
 export { default as Public } from './public';
 export { default as Header } from './Header';
+export { default as SidePanelContent } from './SidePanelContent';
 
 /**
  * @NOTE
