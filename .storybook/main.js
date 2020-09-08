a406e36747834a61ee07449e3cdd361a5454aee3
Takuya Hirata <takuyahirata4@gmail.com>
2020-09-08 15:35:28 -0400

    fix: babel-config

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,9 +2,16 @@ module.exports = {
   addons: [
     '@storybook/addon-actions/register',
     '@storybook/addon-a11y/register',
-    '@storybook/addon-docs',
     '@storybook/addon-knobs/register',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        mdxBabelOptions: {
+          babelrc: true,
+          configFile: true,
+        },
+      },
+    },
   ],
-
   stories: ['../packages/**/*.stories.mdx'],
 };
