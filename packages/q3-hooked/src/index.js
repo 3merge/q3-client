73338c6cac80570f9ab1d828bbc492c130fddacd
Takuya Hirata <takuyahirata4@gmail.com>
2020-11-06 15:41:38 -0500

    chore: setup

--- packages/q3-hooked/src/index.js
+++ packages/q3-hooked/src/index.js
@@ -1,2 +1,3 @@
 export { default as useBack } from './useBack';
 export { default as useGatekeeper } from './useGatekeeper';
+export { default as useNavigation } from './useNavigation';
---
+++ packages/q3-hooked/src/useNavigation/useNavigation.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const useNavigation = () => {};
+
+export default useNavigation;
