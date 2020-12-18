745c9871dd7bbe740053a35a152571ea359702ce
Mike <mibberson@3merge.ca>
2020-12-18 14:55:51 -0500

    chore(wip): new package setup

--- packages/q3-admin/src/containers/trash/index.jsx
+++ packages/q3-admin/src/containers/trash/index.jsx
@@ -11,80 +11,82 @@ import Graphic from 'q3-ui-assets';
 import { Throw } from 'q3-ui-assets';
 import { useAuth } from 'q3-ui-permissions';
 import { browser } from 'q3-ui-helpers';
+import { Page } from 'q3-ui-trash';
 import connect from '../connect';
 
-export const Trash = ({
-  collectionName,
-  onDelete,
-  directoryPath,
-}) => {
-  const [loading, setLoading] = React.useState(false);
-  const [showError, setShowError] = React.useState(false);
-  const [showRedirect, setShowRedirect] = React.useState(
-    false,
-  );
+// export const Trash = ({
+//   collectionName,
+//   onDelete,
+//   directoryPath,
+// }) => {
+//   const [loading, setLoading] = React.useState(false);
+//   const [showError, setShowError] = React.useState(false);
+//   const [showRedirect, setShowRedirect] = React.useState(
+//     false,
+//   );
 
-  const { t } = useTranslation();
-  const { canDelete } = useAuth(collectionName);
+//   const { t } = useTranslation();
+//   const { canDelete } = useAuth(collectionName);
 
-  const navigateOnResolve = () =>
-    onDelete()
-      .then(() => {
-        setShowRedirect(true);
-        browser.redirectIn(directoryPath);
-      })
-      .catch(() => {
-        setShowError(true);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+//   const navigateOnResolve = () =>
+//     onDelete()
+//       .then(() => {
+//         setShowRedirect(true);
+//         browser.redirectIn(directoryPath);
+//       })
+//       .catch(() => {
+//         setShowError(true);
+//       })
+//       .finally(() => {
+//         setLoading(false);
+//       });
 
-  return (
-    <>
-      {showError && (
-        <Alert
-          type="error"
-          label={t('descriptions:trashFail')}
-          dismissable={false}
-        />
-      )}
-      {showRedirect && (
-        <Alert
-          type="success"
-          label={t('descriptions:trashSuccess')}
-          dismissable={false}
-        />
-      )}
-      <Graphic
-        description="trashDescription"
-        icon="Throw"
-        renderBottom={() => (
-          <Box mt={2}>
-            {loading ? (
-              <CircularProgress />
-            ) : (
-              <Button
-                variant="contained"
-                color="primary"
-                size="large"
-                onClick={navigateOnResolve}
-                disabled={!canDelete}
-              >
-                {t('labels:addToTrash')}
-              </Button>
-            )}
-          </Box>
-        )}
-      />
-    </>
-  );
-};
+//   return (
+//     <>
 
-Trash.propTypes = {
-  collectionName: PropTypes.string.isRequired,
-  onDelete: PropTypes.func.isRequired,
-  directoryPath: PropTypes.string.isRequired,
-};
+//       {showError && (
+//         <Alert
+//           type="error"
+//           label={t('descriptions:trashFail')}
+//           dismissable={false}
+//         />
+//       )}
+//       {showRedirect && (
+//         <Alert
+//           type="success"
+//           label={t('descriptions:trashSuccess')}
+//           dismissable={false}
+//         />
+//       )}
+//       <Graphic
+//         description="trashDescription"
+//         icon="Throw"
+//         renderBottom={() => (
+//           <Box mt={2}>
+//             {loading ? (
+//               <CircularProgress />
+//             ) : (
+//               <Button
+//                 variant="contained"
+//                 color="primary"
+//                 size="large"
+//                 onClick={navigateOnResolve}
+//                 disabled={!canDelete}
+//               >
+//                 {t('labels:addToTrash')}
+//               </Button>
+//             )}
+//           </Box>
+//         )}
+//       />
+//     </>
+//   );
+// };
 
-export default connect(Trash);
+// Trash.propTypes = {
+//   collectionName: PropTypes.string.isRequired,
+//   onDelete: PropTypes.func.isRequired,
+//   directoryPath: PropTypes.string.isRequired,
+// };
+
+export default connect(Page);
