27843b500252f2238391609a7558b7ff62e89d23
Mike <mibberson@3merge.ca>
2021-01-05 11:48:37 -0500

    test: search navigation

---
+++ packages/q3-admin/src/components/Search/Search.test.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { useNavigate, useLocation } from '@reach/router';
+import Search from './Search';
+import SearchFullWidth from '../SearchFullWidth';
+
+jest.mock('@reach/router', () => ({
+  useNavigate: jest.fn(),
+  useLocation: jest.fn(),
+}));
+
+describe('Search', () => {
+  it('should set default value by search', () => {
+    useLocation.mockReturnValue({
+      search: '?search=foo',
+    });
+
+    expect(
+      global
+        .shallow(<Search />)
+        .find(SearchFullWidth)
+        .props(),
+    ).toHaveProperty('defaultValue', 'foo');
+  });
+
+  it('should set placeholder value by prop', () =>
+    expect(
+      global
+        .shallow(<Search collectionName="orders" />)
+        .find(SearchFullWidth)
+        .props(),
+    ).toHaveProperty(
+      'placeholder',
+      'ordersSearchPlaceholder',
+    ));
+
+  it('should navigate on enter', () => {
+    const navigate = jest.fn();
+    useNavigate.mockReturnValue(navigate);
+    useLocation.mockReturnValue({
+      pathname: 'root',
+    });
+
+    global
+      .shallow(<Search collectionName="orders" />)
+      .find(SearchFullWidth)
+      .prop('onKeyPress')({
+      code: 'Enter',
+      target: {
+        value: 'test',
+      },
+    });
+
+    expect(navigate).toHaveBeenCalledWith(
+      'root/?search=test',
+    );
+  });
+});
