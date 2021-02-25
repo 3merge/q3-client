e5ad273e95af1298bcb8b3c7df1e75076c449b15
Takuya Hirata <39467028+Takuya-H23@users.noreply.github.com>
2021-02-25 15:11:22 -0500

    refactor: timeline ui for visualizing diff
    * feat(wip): Timeline
    
    * feat: timeline
    
    * wip: remove old files
    
    * test: Timeline
    
    * chore: revert babel
    
    * refactor: flatten diff
    
    Co-authored-by: Mike <mibberson@3merge.ca>

--- packages/q3-components/src/Timeline/Timeline.jsx
+++ packages/q3-components/src/Timeline/Timeline.jsx
@@ -1,127 +1,68 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Skeleton from '@material-ui/lab/Skeleton';
-import moment from 'moment';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import { useTranslation } from 'react-i18next';
-import useStyle from './useStyle';
+import { Timeline as MaterialTimeline } from '@material-ui/lab';
+import { string } from 'q3-ui-helpers';
+import { map, size } from 'lodash';
+import {
+  TimelineContent,
+  TimelineItem,
+  TimelineSeparator,
+  TimelineConnector,
+  TimelineOppositeContent,
+  TimelineDot,
+} from '@material-ui/lab';
+import { Typography } from '@material-ui/core';
+import useStyles from './useStyle';
+import TimelineEntry from '../TimelineEntry';
 
-const isBase = (target) => target === 'baseSchema';
+const hasDiff = (a) => size(a.diff) && a.modifiedOn;
 
-const formatPrevCurrDescription = ({ prev, curr }) =>
-  prev
-    ? `changed from "${prev}" to "${curr}"`
-    : `set to "${curr}"`;
+const Timeline = ({ entries, fetching }) => {
+  const cls = useStyles();
+  const data = Array.isArray(entries)
+    ? entries.filter(hasDiff)
+    : [];
 
-const formatNestedProperty = (target, name) =>
-  name && name.includes('%2E') && !isBase(target)
-    ? name.replace(target, '').replace(/(%2E)\d+(%2E)/, '')
-    : name;
-
-const getColor = (op) => {
-  switch (op) {
-    case 'Create':
-      return 'green';
-    case 'Delete':
-      return 'red';
-    case 'Update':
-      return 'purple';
-    default:
-      return 'blue';
-  }
-};
-
-const getUser = (modifiedBy) => {
-  if (!modifiedBy) return 'Untracked user';
-  return `${modifiedBy.firstName} ${modifiedBy.lastName}`;
-};
-
-export const TimelineListItemWrapper = ({
-  children,
-  op,
-}) => {
-  const { item } = useStyle({ color: getColor(op) });
+  if (fetching || data.length === 0) return null;
 
   return (
-    <Box component="li" className={item}>
-      <div>{children}</div>
-    </Box>
+    <MaterialTimeline>
+      {data.map(({ modifiedOn, modifiedBy, diff }, idx) => (
+        <TimelineItem
+          key={`timelineItem${idx}`}
+          className={cls.wrapper}
+        >
+          <TimelineOppositeContent className={cls.initial}>
+            <Typography className={cls.date}>
+              <i>{string.toDate(modifiedOn)}</i>
+            </Typography>
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineDot />
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent>
+            <small>
+              <u>{modifiedBy || 'Sys'}</u>
+            </small>
+            {map(diff, (entry, i) => (
+              <TimelineEntry key={i} data={entry} />
+            ))}
+          </TimelineContent>
+        </TimelineItem>
+      ))}
+    </MaterialTimeline>
   );
 };
 
-const getDescription = (op, resource, t) => {
-  if (isBase(resource))
-    return `${op.toLowerCase()}d the document`;
-
-  if (op === 'Delete')
-    return `remove an entry from ${t(resource)}`;
-  if (op === 'Create') return `added to ${t(resource)}`;
-  return `updated entries in ${t(resource)}`;
+Timeline.defaultProps = {
+  fetching: false,
 };
 
-const TimelineListItem = ({
-  description,
-  modifiedOn,
-  modifiedBy,
-  modified,
-  target,
-  op,
-}) => {
-  const { t } = useTranslation('labels');
-  return op ? (
-    <TimelineListItemWrapper op={op}>
-      <Grid container spacing={1}>
-        <Grid item>
-          <Typography style={{ margin: 0 }}>
-            {`${getUser(modifiedBy)} ${getDescription(
-              op,
-              target,
-              t,
-            )} on ${moment(modifiedOn).format('LLLL')}`}
-          </Typography>
-          <Typography component="ul">
-            {Object.entries(modified).map(
-              ([key, value]) => (
-                <li key={key}>
-                  <strong>
-                    <u>
-                      {t(formatNestedProperty(target, key))}
-                    </u>
-                  </strong>{' '}
-                  {formatPrevCurrDescription(value)}
-                </li>
-              ),
-            )}
-          </Typography>
-        </Grid>
-      </Grid>
-    </TimelineListItemWrapper>
-  ) : null;
+Timeline.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  entries: PropTypes.array.isRequired,
+  fetching: PropTypes.bool,
 };
 
-export const TimelineSkeleton = (props) => (
-  <TimelineListItemWrapper {...props}>
-    <Skeleton
-      variant="rect"
-      height={45}
-      width="100%"
-      style={{ marginBottom: '0.5rem' }}
-    />
-    <Skeleton variant="rect" height={85} width="100%" />
-  </TimelineListItemWrapper>
-);
-
-export default ({ entries, fetching }) => {
-  const { root } = useStyle();
-  return (
-    <Box className={root} component="ul">
-      {fetching
-        ? null
-        : entries.map(({ ref: id, ...etc }) => (
-            <TimelineListItem key={id} {...etc} />
-          ))}
-    </Box>
-  );
-};
+export default Timeline;
