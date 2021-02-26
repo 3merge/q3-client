797aa508ca368ffb199294f7d91c6a3c69307ad0
Mike Ibberson <32675502+MikeIbberson@users.noreply.github.com>
2021-02-26 18:15:50 -0500

    refactor: timeline component (#299)

--- packages/q3-components/src/Timeline/Timeline.jsx
+++ packages/q3-components/src/Timeline/Timeline.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Timeline as MaterialTimeline } from '@material-ui/lab';
-import { string } from 'q3-ui-helpers';
-import { size } from 'lodash';
 import {
   TimelineContent,
   TimelineItem,
@@ -11,35 +9,60 @@ import {
   TimelineOppositeContent,
   TimelineDot,
 } from '@material-ui/lab';
+import { string } from 'q3-ui-helpers';
+import ReactDiffViewer, {
+  DiffMethod,
+} from 'react-diff-viewer';
 import { Typography } from '@material-ui/core';
+import {
+  omit,
+  get,
+  map,
+  isObject,
+  join,
+  compact,
+} from 'lodash';
 import useStyles from './useStyle';
-import TimelineEntry from '../TimelineEntry';
 
-const hasDiff = (a) =>
-  (size(a.updatedFields) || size(a.removedFields)) &&
-  a.modifiedOn;
+const removeMeta = (o) =>
+  JSON.stringify(
+    omit(o, ['lastModifiedBy', 'updatedAt']),
+    null,
+    2,
+  );
+
+const getName = (o) =>
+  isObject(o)
+    ? join(
+        compact(
+          map(['firstName', 'lastName'], (v) => get(o, v)),
+        ),
+        ' ',
+      )
+    : 'System';
 
 const Timeline = ({ entries, fetching }) => {
   const cls = useStyles();
-  const data = Array.isArray(entries)
-    ? entries.filter(hasDiff)
-    : [];
-
+  const data = Array.isArray(entries) ? entries : [];
   if (fetching || data.length === 0) return null;
 
   return (
     <MaterialTimeline>
-      {data.map(
-        ({ modifiedOn, modifiedBy, ...rest }, idx) => (
+      {data.map((item, i) =>
+        i !== 0 ? (
           <TimelineItem
-            key={`timelineItem${idx}`}
+            key={`timelineItem${i}`}
             className={cls.wrapper}
           >
             <TimelineOppositeContent
               className={cls.initial}
             >
               <Typography className={cls.date}>
-                <i>{string.toDate(modifiedOn)}</i>
+                <strong>
+                  {getName(item.lastModifiedBy)}
+                </strong>
+                <br />
+                {string.toDate(item.updatedAt)}
               </Typography>
             </TimelineOppositeContent>
             <TimelineSeparator>
@@ -47,13 +70,15 @@ const Timeline = ({ entries, fetching }) => {
               <TimelineConnector />
             </TimelineSeparator>
             <TimelineContent>
-              <small>
-                <u>{modifiedBy || 'Sys'}</u>
-              </small>
-              <TimelineEntry data={rest} />
+              <ReactDiffViewer
+                newValue={removeMeta(data[i - 1])}
+                oldValue={removeMeta(item)}
+                compareMethod={DiffMethod.WORDS}
+                splitView
+              />
             </TimelineContent>
           </TimelineItem>
-        ),
+        ) : null,
       )}
     </MaterialTimeline>
   );
--- packages/q3-components/src/Timeline/Timeline.test.jsx
+++ packages/q3-components/src/Timeline/Timeline.test.jsx
@@ -1,6 +1,37 @@
+import React from 'react';
+import {
+  TimelineContent,
+  TimelineSeparator,
+  TimelineOppositeContent,
+} from '@material-ui/lab';
+import Timeline from './Timeline';
+import data from './data';
+
 describe('Timeline', () => {
-  it.todo('should block rendering while fetching');
-  it.todo('should filter timeline entries');
-  it.todo('should block rendering without entries');
-  it.todo('should block render timeline');
+  it('should block rendering while fetching', () => {
+    expect(
+      global
+        .shallow(<Timeline entries={[]} fetching />)
+        .isEmptyRender(),
+    ).toBeTruthy();
+  });
+
+  it('should block rendering without entries', () => {
+    expect(
+      global
+        .shallow(<Timeline entries={[]} />)
+        .isEmptyRender(),
+    ).toBeTruthy();
+  });
+
+  it('should block render timeline', () => {
+    const res = global.mount(<Timeline entries={data} />);
+    [
+      TimelineContent,
+      TimelineSeparator,
+      TimelineOppositeContent,
+    ].forEach((el) =>
+      expect(res.find(el).exists).toBeTruthy(),
+    );
+  });
 });
