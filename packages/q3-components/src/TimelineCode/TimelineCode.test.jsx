b946d87e9df47080bb8991d8338c44932050ad67
Takuya Hirata <takuyahirata4@gmail.com>
2021-02-26 10:34:54 -0500

    test: TimelineCode

--- packages/q3-components/src/TimelineCode/TimelineCode.test.jsx
+++ packages/q3-components/src/TimelineCode/TimelineCode.test.jsx
@@ -1,4 +1,19 @@
+import React from 'react';
+import { red, blue } from '@material-ui/core/colors';
+import TimelineCode from './TimelineCode';
+
+const getColor = (label) =>
+  global
+    .shallow(<TimelineCode value="foobar" label={label} />)
+    .find('span')
+    .prop('style').color;
+
 describe('TimelineCode', () => {
-  it.todo('should colour red');
-  it.todo('should colour blue');
+  it('should colour red', () => {
+    expect(getColor('unset')).toBe(red[900]);
+  });
+
+  it('should colour blue', () => {
+    expect(getColor('john')).toBe(blue[900]);
+  });
 });
