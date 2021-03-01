25f5c20389c6d55164ef3603cb4f8d03d26b87d6
Takuya Hirata <takuyahirata4@gmail.com>
2021-03-01 16:40:13 -0500

    fix: revert babel

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,22 @@
-const path = require('path');
+const os = require('os');
 
 const withPackageOpts = (s) =>
-  path.resolve(__dirname, `./packages/${s}/src`);
+  os.type() !== 'Darwin'
+    ? `./packages/${s}/src`
+    : `/packages/${s}/src`;
+
+const withPackageTests = (s) => `./packages/${s}/tests`;
+
+const withTests = (s) => `${s}/tests`;
 const withBundledDir = (s) => `${s}/lib`;
 
 const alias = [
   'q3-admin',
   'q3-blocks',
   'q3-components',
-  'q3-hooked',
   'q3-ui-assets',
+  'q3-ui-charts',
+  'q3-ui-confirm',
   'q3-ui-locale',
   'q3-ui',
   'q3-ui-datatables',
@@ -19,18 +26,18 @@ const alias = [
   'q3-ui-forms',
   'q3-ui-helpers',
   'q3-ui-notifications',
+  'q3-ui-queryparams',
   'q3-ui-permissions',
   'q3-ui-repeater',
   'q3-ui-test-utils',
   'q3-ui-rest',
-].reduce(
-  (acc, curr) =>
-    Object.assign(acc, {
-      [withBundledDir(curr)]: withPackageOpts(curr),
-      [curr]: withPackageOpts(curr), // default exports
-    }),
-  {},
-);
+].reduce((acc, curr) => {
+  return Object.assign(acc, {
+    [withBundledDir(curr)]: withPackageOpts(curr),
+    [withTests(curr)]: withPackageTests(curr),
+    [curr]: withPackageOpts(curr), // default exports
+  });
+}, {});
 
 const withAlias = {
   plugins: [
@@ -49,6 +56,7 @@ const config = {
   plugins: [
     '@babel/plugin-proposal-export-namespace-from',
     '@babel/plugin-transform-modules-commonjs',
+    '@babel/plugin-proposal-optional-chaining',
   ],
   presets: [
     [
