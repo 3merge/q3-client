f1cb457668dba4bddb0ab9a6d69004e54ebf3c82
Mike <mibberson@3merge.ca>
2021-04-27 15:33:55 -0400

    chore: lazy load quill to avoid ssr issues

--- packages/q3-ui-rte/src/components/index.js
+++ packages/q3-ui-rte/src/components/index.js
@@ -1,1 +1,10 @@
-export { default } from './RichTextEditor';
+import React from 'react';
+
+const Editor = React.lazy(() => import('./RichTextEditor'));
+
+export default (props) => (
+  // eslint-disable-next-line
+  <React.Suspense fallback={<div />}>
+    <Editor {...props} />
+  </React.Suspense>
+);
