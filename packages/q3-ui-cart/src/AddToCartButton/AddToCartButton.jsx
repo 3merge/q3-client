254b9bb175c86d29950151ffe2200994aef9f281
Mike Ibberson <32675502+MikeIbberson@users.noreply.github.com>
2021-06-04 09:09:08 -0400

    chore: added props to cart and field (#328)
    * chore: added props to cart and field
    
    * chore: add disabled prop

--- packages/q3-ui-cart/src/AddToCartButton/AddToCartButton.jsx
+++ packages/q3-ui-cart/src/AddToCartButton/AddToCartButton.jsx
@@ -12,7 +12,12 @@ import { ADD_TO_CART_CLASS } from '../constants';
 import useStyle, { getFromProps } from './useStyle';
 import useReset from './useReset';
 
-const AddToCart = ({ quantity = 1, product }) => {
+const AddToCart = ({
+  quantity = 1,
+  product,
+  disabled: isDisabled,
+  ...rest
+}) => {
   const { add } = React.useContext(CartContext);
   const { t } = useTranslation('labels');
   const [loading, setLoading] = React.useState(false);
@@ -32,6 +37,7 @@ const AddToCart = ({ quantity = 1, product }) => {
   const onClick = React.useCallback(() => {
     setLoading(true);
     return add({
+      ...rest,
       product,
       quantity,
     })
@@ -47,7 +53,7 @@ const AddToCart = ({ quantity = 1, product }) => {
       fullWidth
       className={classnames(ADD_TO_CART_CLASS, btn)}
       onClick={onClick}
-      disabled={disabled}
+      disabled={disabled || isDisabled}
       variant="contained"
       size="large"
       color="primary"
@@ -64,9 +70,14 @@ const AddToCart = ({ quantity = 1, product }) => {
   );
 };
 
+AddToCart.defaultProps = {
+  disabled: false,
+};
+
 AddToCart.propTypes = {
   quantity: PropTypes.number.isRequired,
   product: PropTypes.string.isRequired,
+  disabled: PropTypes.bool,
 };
 
 export default AddToCart;
