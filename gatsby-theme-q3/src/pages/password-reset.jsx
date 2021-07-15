31a5b112c226d934fe75bab9b54afd9c6efd8dfd
Mike Ibberson <32675502+MikeIbberson@users.noreply.github.com>
2021-07-15 10:39:41 -0400

    refactor: use success notice on password utilities (#333)
    * chore: success op for password utilities
    
    * chore: change debounce on rte

--- gatsby-theme-q3/src/pages/password-reset.jsx
+++ gatsby-theme-q3/src/pages/password-reset.jsx
@@ -1,72 +1,45 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
-import { Link } from 'gatsby';
 import axios from 'axios';
 import { Form, Field } from 'q3-ui-forms/lib/builders';
-import Button from '@material-ui/core/Button';
 import FormBox from '../components/FormBox';
 import FormBoxContent from '../components/FormBoxContent';
-import FormBoxNotice from '../components/FormBoxNotice';
-import { hasOp, toOp } from '../components/utils';
+import withSuccessOp from '../components/withSuccessOp';
 
-const PasswordReset = ({
-  location: { search, pathname },
-}) => {
-  const { t } = useTranslation();
-
-  if (hasOp(search))
-    return (
-      <FormBoxNotice
-        title="passwordResetNotice"
-        description="passwordResetNotice"
+const PasswordReset = ({ onSuccess }) => (
+  <FormBox
+    renderBottom={
+      <Form
+        onSubmit={(body) =>
+          axios
+            .post('/password-reset', body)
+            .then(onSuccess)
+        }
       >
-        <Button
-          component={Link}
-          to="/login"
-          variant="contained"
-          color="primary"
-        >
-          {t('labels:login')}
-        </Button>
-      </FormBoxNotice>
-    );
-
-  return (
-    <FormBox
-      renderBottom={
-        <Form
-          onSubmit={(body) =>
-            axios
-              .post('/password-reset', body)
-              .then(toOp(pathname))
-          }
-        >
-          <Field
-            type="email"
-            name="email"
-            xl={12}
-            lg={12}
-            md={12}
-            required
-          />
-        </Form>
-      }
-      renderTop={
-        <FormBoxContent
-          title="passwordReset"
-          description="passwordReset"
+        <Field
+          type="email"
+          name="email"
+          xl={12}
+          lg={12}
+          md={12}
+          required
         />
-      }
-    />
-  );
-};
+      </Form>
+    }
+    renderTop={
+      <FormBoxContent
+        title="passwordReset"
+        description="passwordReset"
+      />
+    }
+  />
+);
 
 PasswordReset.propTypes = {
-  location: PropTypes.shape({
-    search: PropTypes.string,
-    pathname: PropTypes.string,
-  }).isRequired,
+  onSuccess: PropTypes.func.isRequired,
 };
 
-export default PasswordReset;
+export default withSuccessOp(
+  PasswordReset,
+  'passwordResetNotice',
+);
