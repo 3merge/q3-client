1570670de3b714d9373d7aac7093d19a20c50248
Mike <mibberson@3merge.ca>
2021-08-09 17:17:57 -0400

    chore(wip): configure logging filters

--- packages/q3-ui-audit/src/components/Audit/Audit.jsx
+++ packages/q3-ui-audit/src/components/Audit/Audit.jsx
@@ -1,58 +1,58 @@
 import React from 'react';
-import { isObject, get, pick } from 'lodash';
+import {
+  Button,
+  Container,
+  Grid,
+  Box,
+} from '@material-ui/core';
+import { Builders } from 'q3-ui-forms';
 import Timeline from '../Timeline';
 
-const compareChangeLogWithCurrent = (xs = {}) => (
-  target,
-) => {
-  try {
-    const firstKey = Object.keys(target)[0];
-
-    if (
-      Object.keys(target).length === 1 &&
-      isObject(target[firstKey])
-    ) {
-      const nested = get(xs, firstKey);
-
-      if (Array.isArray(nested)) {
-        return JSON.stringify(
-          {
-            [firstKey]: nested.find(
-              (item) =>
-                item.id ===
-                (target[firstKey]._id ||
-                  target[firstKey].id),
-            ),
-          },
-          null,
-          2,
-        );
-      }
-
-      return JSON.stringify(
-        { [firstKey]: nested },
-        null,
-        2,
-      );
-    }
-
-    return JSON.stringify(
-      pick(xs, Object.keys(target)),
-      null,
-      2,
-    );
-  } catch (e) {
-    return JSON.stringify({}, null, 2);
-  }
-};
-
-const Audit = ({ current, data }) => (
-  <Timeline
-    compareChangeLogWithCurrent={compareChangeLogWithCurrent(
-      current,
-    )}
-    data={data}
-  />
+const Audit = (props) => (
+  <Container>
+    <Builders.Form enableSubmit={false}>
+      <Grid item xs={12}>
+        <Grid container spacing={1}>
+          <Grid item xs>
+            <Grid container spacing={1}>
+              <Builders.Field
+                name="user"
+                type="date"
+                helperText={
+                  'Currently, querying by "System" is not possible'
+                }
+              />
+              <Grid item xs={6} lg={6}>
+                <Grid container spacing={1}>
+                  <Builders.Field
+                    name="date"
+                    type="date"
+                    helperText="Will only query logs before the requested date"
+                  />
+                  <Builders.Field
+                    name="operations"
+                    type="multiselect"
+                    options={[
+                      'added',
+                      'deleted',
+                      'updated',
+                    ]}
+                  />
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item style={{ width: 'auto' }}>
+            <Builders.Next submit label="apply" />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Builders.Form>
+    <Timeline {...props} />
+    <Box align="center" my={2}>
+      <Button disabled>Load more</Button>
+    </Box>
+  </Container>
 );
 
 export default Audit;
