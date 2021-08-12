bd1944ffd7ec063518c1fdaeeb0321ee869abc36
Mike <mibberson@3merge.ca>
2021-08-12 16:23:38 -0400

    style: dark mode

--- packages/q3-ui-audit/src/components/TimelineDiff/TimelineDiff.jsx
+++ packages/q3-ui-audit/src/components/TimelineDiff/TimelineDiff.jsx
@@ -14,6 +14,8 @@ import ReactDiffViewer, {
 import { useTranslation } from 'react-i18next';
 import { useTheme } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { green, red } from '@material-ui/core/colors';
+import { fade } from '@material-ui/core/styles/colorManipulator';
 
 const TimelineDiff = ({ prev, next, children }) => {
   const { state, toggle } = useToggle();
@@ -42,9 +44,123 @@ const TimelineDiff = ({ prev, next, children }) => {
                 oldValue={prev}
                 newValue={next}
                 compareMethod={DiffMethod.WORDS}
+                hideLineNumbers
                 useDarkTheme={
                   theme?.palette?.type === 'dark'
                 }
+                styles={{
+                  variables: {
+                    light: {
+                      diffViewerBackground:
+                        theme.palette.background.paper,
+
+                      addedBackground: fade(
+                        green[100],
+                        0.5,
+                      ),
+                      addedColor: green[900],
+                      removedBackground: fade(
+                        red[100],
+                        0.5,
+                      ),
+                      removedColor: red[900],
+                      wordAddedBackground: fade(
+                        green[300],
+                        0.8,
+                      ),
+                      wordRemovedBackground: fade(
+                        red[300],
+                        0.8,
+                      ),
+                      addedGutterBackground:
+                        theme.palette.background.default,
+                      removedGutterBackground:
+                        theme.palette.background.default,
+                      gutterBackground:
+                        theme.palette.background.default,
+                      gutterBackgroundDark:
+                        theme.palette.background.default,
+                      highlightBackground:
+                        theme.palette.background.default,
+                      highlightGutterBackground:
+                        theme.palette.background.default,
+                      codeFoldGutterBackground:
+                        theme.palette.background.default,
+                      codeFoldBackground:
+                        theme.palette.background.default,
+                      emptyLineBackground:
+                        theme.palette.background.default,
+                      gutterColor:
+                        theme.palette.primary.main,
+                      addedGutterColor:
+                        theme.palette.primary.main,
+                      removedGutterColor:
+                        theme.palette.primary.main,
+                      codeFoldContentColor:
+                        theme.palette.primary.main,
+                      diffViewerTitleBackground:
+                        theme.palette.background.paper,
+                      diffViewerTitleColor:
+                        theme.palette.primary.main,
+                      diffViewerTitleBorderColor:
+                        theme.palette.background.paper,
+                    },
+                    dark: {
+                      diffViewerBackground:
+                        theme.palette.background.paper,
+                      addedBackground: fade(
+                        green[50],
+                        0.15,
+                      ),
+                      addedColor: green[100],
+                      removedBackground: fade(
+                        red[50],
+                        0.15,
+                      ),
+                      removedColor: red[100],
+                      wordAddedBackground: fade(
+                        green[900],
+                        0.8,
+                      ),
+                      wordRemovedBackground: fade(
+                        red[900],
+                        0.8,
+                      ),
+                      addedGutterBackground:
+                        theme.palette.background.default,
+                      removedGutterBackground:
+                        theme.palette.background.default,
+                      gutterBackground:
+                        theme.palette.background.default,
+                      gutterBackgroundDark:
+                        theme.palette.background.default,
+                      highlightBackground:
+                        theme.palette.background.default,
+                      highlightGutterBackground:
+                        theme.palette.background.default,
+                      codeFoldGutterBackground:
+                        theme.palette.background.default,
+                      codeFoldBackground:
+                        theme.palette.background.default,
+                      emptyLineBackground:
+                        theme.palette.background.default,
+                      gutterColor:
+                        theme.palette.primary.main,
+                      addedGutterColor:
+                        theme.palette.primary.main,
+                      removedGutterColor:
+                        theme.palette.primary.main,
+                      codeFoldContentColor:
+                        theme.palette.primary.main,
+                      diffViewerTitleBackground:
+                        theme.palette.background.paper,
+                      diffViewerTitleColor:
+                        theme.palette.primary.contrastColor,
+                      diffViewerTitleBorderColor:
+                        theme.palette.background.paper,
+                    },
+                  },
+                }}
                 {...(matches
                   ? {
                       splitView: false,
