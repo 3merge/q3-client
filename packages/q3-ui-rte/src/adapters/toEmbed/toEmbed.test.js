8168358af76392a2ff28e26450ee062f6037bdf1
Mike <mibberson@3merge.ca>
2021-08-16 14:23:12 -0400

    fix: video embed and audit filtering

--- packages/q3-ui-rte/src/adapters/toEmbed/toEmbed.test.js
+++ packages/q3-ui-rte/src/adapters/toEmbed/toEmbed.test.js
@@ -3,11 +3,11 @@ import toEmbed from './toEmbed';
 test.each([
   [
     'http://home.wistia.com/medias/e4a27b971d',
-    'http://fast.wistia.net/embed/iframe/e4a27b971d?version=v1',
+    '//fast.wistia.net/embed/iframe/e4a27b971d?version=v1',
   ],
   [
     'https://www.youtube.com/watch?v=I1188GO4p1E',
-    'http://www.youtube.com/embed/I1188GO4p1E',
+    '//www.youtube.com/embed/I1188GO4p1E',
   ],
 ])('.toEmbed(%s)', (a, expected) => {
   expect(toEmbed(a)).toBe(expected);
