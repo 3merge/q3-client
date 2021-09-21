9cc3bdc292476e357fc7791089ece8158c15de92
Mike <mibberson@3merge.ca>
2021-09-21 15:55:20 -0400

    test: rest email hook

---
+++ packages/q3-ui-emaileditor/src/components/useEmailTemplates/useEmailTemplates.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { get, find, size } from 'lodash';
+import useRest from 'q3-ui-rest';
+
+export const isPartial = (xs) =>
+  !xs?.name || String(xs.name).startsWith('__');
+
+export const findById = (xs, id) =>
+  find(xs, (item) => item?.id === id);
+
+export const getFirstFullTemplateId = (xs) =>
+  find(xs, (item) => !isPartial(item))?.id;
+
+const useEmailTemplates = () => {
+  const {
+    emails = [],
+    fetchingError = false,
+    fetching = true,
+    patch,
+  } = useRest({
+    key: 'email',
+    pluralized: 'emails',
+    url: '/emails',
+    runOnInit: true,
+    location: {
+      search: '?sort=name',
+    },
+  });
+
+  const [active, setActive] = React.useState();
+  const current = findById(emails, active);
+
+  const handleSave = (mjml) =>
+    patch(active)({
+      mjml,
+    });
+
+  React.useEffect(() => {
+    if (size(emails) && !active)
+      setActive(getFirstFullTemplateId(emails));
+  }, [emails]);
+
+  return {
+    templates: emails,
+    error: Boolean(fetchingError || !size(emails)),
+    ready: Boolean(!fetching && current),
+    disablePreview: isPartial(current),
+    value: get(current, 'mjml', '<mjml />'),
+    vars: get(current, 'variables', {}),
+    onSave: handleSave,
+    setById: setActive,
+  };
+};
+
+export default useEmailTemplates;
