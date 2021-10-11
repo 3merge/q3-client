3748086da42bd1f1ecebad74608c3a785bab38d7
Mike <mibberson@3merge.ca>
2021-10-11 10:28:59 -0400

    fix: ssr

--- gatsby-theme-q3/src/components/RedirectToIndex.jsx
+++ gatsby-theme-q3/src/components/RedirectToIndex.jsx
@@ -1,10 +1,15 @@
 // eslint-disable-next-line
 import React from 'react';
 import { navigate } from 'gatsby';
+import { browser } from 'q3-ui-helpers';
 import useSiteMetaData from './useSiteMetaData';
 
 const IndexPage = () => {
-  navigate(useSiteMetaData().appDirectory);
+  React.useEffect(() => {
+    if (browser.isBrowserReady())
+      navigate(useSiteMetaData().appDirectory);
+  }, []);
+
   return null;
 };
 
