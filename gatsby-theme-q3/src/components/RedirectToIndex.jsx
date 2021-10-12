79641d263282868ff7d344dcd74eb340313a5e14
Mike <mibberson@3merge.ca>
2021-10-12 08:26:38 -0400

    fix: invalid hook

--- gatsby-theme-q3/src/components/RedirectToIndex.jsx
+++ gatsby-theme-q3/src/components/RedirectToIndex.jsx
@@ -1,13 +1,12 @@
-// eslint-disable-next-line
 import React from 'react';
 import { navigate } from 'gatsby';
-import { browser } from 'q3-ui-helpers';
 import useSiteMetaData from './useSiteMetaData';
 
 const IndexPage = () => {
+  const d = useSiteMetaData().appDirectory;
+
   React.useEffect(() => {
-    if (browser.isBrowserReady())
-      navigate(useSiteMetaData().appDirectory);
+    navigate(d);
   }, []);
 
   return null;
