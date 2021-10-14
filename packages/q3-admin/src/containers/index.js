5f47d9e4a43dda42229653c2b945460bdcc46378
Mike <mibberson@3merge.ca>
2021-10-14 18:02:00 -0400

    chore: delete a bunch of unused components

--- packages/q3-admin/src/containers/index.js
+++ packages/q3-admin/src/containers/index.js
@@ -3,7 +3,6 @@ export { default as AddWrapper } from './AddWrapper';
 export { default as Detail } from './detail';
 export { default as Filters } from './filters';
 export { default as Gatekeeper } from './gatekeeper';
-export { default as Header } from './header';
 export { default as Table } from './table';
 export { default as Page } from './page';
 export { default as SubDetail } from './subDetail';
