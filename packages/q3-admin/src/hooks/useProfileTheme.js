9bb6e88b0d325da5414f854fbf765aa50730adb8
Mike <mibberson@3merge.ca>
2022-02-09 09:46:50 -0500

    test: profile hooks

--- packages/q3-admin/src/hooks/useProfileTheme.js
+++ packages/q3-admin/src/hooks/useProfileTheme.js
@@ -3,11 +3,10 @@ import { ModeContext } from 'q3-ui/lib/Mode';
 import { AuthContext } from 'q3-ui-permissions';
 
 const useProfileTheme = () => {
+  const { setType } = React.useContext(ModeContext);
   const type =
     React.useContext(AuthContext)?.state?.profile?.theme ||
     'light';
-
-  const { setType } = React.useContext(ModeContext);
 
   React.useEffect(() => {
     setType(type);
