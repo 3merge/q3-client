d8a89b37dcd6005bcc05fe9b36c9cde13df86c74
Mike <mibberson@3merge.ca>
2022-02-17 00:07:26 -0500

    refactor: unused components

--- packages/q3-blocks/src/__fixtures__/gatsby-image.js
+++
@@ -1,7 +0,0 @@
-export default {
-  title: 'Show me the money!',
-  alt: "Hopefully, you won't see this",
-  fluid: {
-    src: 'https://source.unsplash.com/random',
-  },
-};
--- packages/q3-blocks/src/withLength.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { array } from 'q3-ui-helpers';
-
-export default (Component, propName) => (props) =>
-  array.hasLength(props[propName]) ? (
-    <Component {...props} />
-  ) : null;
