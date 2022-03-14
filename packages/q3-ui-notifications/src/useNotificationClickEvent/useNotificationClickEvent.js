1038c02aa590b06ad0ea26b30f0c8da7b703fc7e
Mike <mibberson@3merge.ca>
2022-03-14 14:25:33 -0400

    fix: urls in notifications

--- packages/q3-ui-notifications/src/useNotificationClickEvent/useNotificationClickEvent.js
+++ packages/q3-ui-notifications/src/useNotificationClickEvent/useNotificationClickEvent.js
@@ -7,6 +7,7 @@ import {
   isNil,
   isString,
   isFunction,
+  some,
 } from 'lodash';
 import { useNavigate } from '@reach/router';
 import { browser } from 'q3-ui-helpers';
@@ -15,28 +16,36 @@ const useNotificationClickEvent = (data, callback) => {
   const [link, setLink] = React.useState();
   const navigate = useNavigate();
 
-  const hasUrl = (xs) => {
-    const url = get(xs, 'url');
-    return !isNil(url) && isString(url) && size(url);
-  };
+  const has =
+    (a = []) =>
+    (xs) =>
+      some(a, (item) => {
+        const v = get(xs, item);
+        return !isNil(v) && isString(v) && size(v);
+      });
 
   const isOutboundUrl = (xs) =>
     xs.startsWith('http') || xs.startsWith('//');
 
-  forEach(filter(data, hasUrl), (item) => {
-    const { acknowledge, hasSeen, url } = item;
+  forEach(
+    filter(data, has(['localUrl', 'url'])),
+    (item) => {
+      const { acknowledge, hasSeen, localUrl, url } = item;
 
-    Object.assign(item, {
-      onClick(e) {
-        e.preventDefault();
-        e.stopPropagation();
+      Object.assign(item, {
+        onClick(e) {
+          e.preventDefault();
+          e.stopPropagation();
 
-        if (!hasSeen && isFunction(acknowledge))
-          acknowledge();
-        setLink(url);
-      },
-    });
-  });
+          if (!hasSeen && isFunction(acknowledge))
+            acknowledge();
+
+          // due to some api changes
+          setLink(localUrl || url);
+        },
+      });
+    },
+  );
 
   React.useEffect(() => {
     if (link && browser.isBrowserReady())
