3d95be7f23796e91f39fbde490c2ca0d565d029b
Mike <mibberson@3merge.ca>
2022-03-31 18:00:53 -0400

    style: table tooltips

--- packages/q3-admin/src/containers/TableLink/TableLink.jsx
+++ packages/q3-admin/src/containers/TableLink/TableLink.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import IconButton from '@material-ui/core/IconButton';
+import { IconButton, Tooltip } from '@material-ui/core';
 import { Link } from '@reach/router';
 import OpenInBrowserIcon from '@material-ui/icons/OpenInBrowser';
+import { useTranslation } from 'q3-ui-locale';
 import { Definitions } from '../state';
 
 const TableLink = ({ id }) => {
   const { rootPath } = React.useContext(Definitions);
+  const { t } = useTranslation('labels');
 
   return (
-    <IconButton
-      color="inherit"
-      component={Link}
-      to={`${rootPath}/${id}`}
-    >
-      <OpenInBrowserIcon />
-    </IconButton>
+    <Tooltip arrow title={t('labels:view')}>
+      <IconButton
+        color="inherit"
+        component={Link}
+        to={`${rootPath}/${id}`}
+      >
+        <OpenInBrowserIcon />
+      </IconButton>
+    </Tooltip>
   );
 };
 
