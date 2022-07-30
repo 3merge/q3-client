519742dbc58821e57fbe29f5f459f3746420c5bd
Mike <mibberson@3merge.ca>
2022-07-30 09:16:46 -0400

    fix: debounce visibility

--- packages/q3-ui-filemanager/src/GalleryItemMedia/GalleryItemMedia.jsx
+++ packages/q3-ui-filemanager/src/GalleryItemMedia/GalleryItemMedia.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  Box,
-  CardMedia,
-  Fade,
-  CircularProgress,
-} from '@material-ui/core';
+import { Box, CardMedia, Fade } from '@material-ui/core';
 import { isObject } from 'lodash';
 import useVisibility from '../useVisibility';
 import { getFileType } from '../utils';
@@ -13,14 +8,11 @@ import useStyle from './styles';
 
 const GalleryItemMedia = ({ children, url }) => {
   const [imageSrc, setImageSrc] = React.useState(null);
-  const [loading, setLoading] = React.useState(true);
   const { isVisible, ref } = useVisibility();
   const fileType = getFileType(url);
   const cls = useStyle({
     fileType,
   });
-
-  const clearLoader = () => setLoading(false);
 
   const handleError = () => {
     setImageSrc(false);
@@ -33,40 +25,29 @@ const GalleryItemMedia = ({ children, url }) => {
         process.env.Q3_THUMBNAIL_API_KEY
       : null;
 
-    if (apiKey && url)
+    if (apiKey && isVisible && url && !imageSrc)
       setImageSrc(
         `https://thumbnails.cloud/v1/jpg?token=${apiKey}&url=${encodeURIComponent(
           url,
         )}`,
       );
-  }, []);
+  }, [isVisible]);
 
   return (
     <CardMedia className={cls.media} ref={ref}>
-      <Fade in={isVisible}>
-        {imageSrc ? (
-          <>
-            <img
-              alt="thumbnail"
-              draggable={false}
-              onLoad={clearLoader}
-              onError={handleError}
-              className={cls.img}
-              src={imageSrc}
-            />
-            {loading && (
-              <Box
-                className={cls.loader}
-                position="absolute"
-              >
-                <CircularProgress />
-              </Box>
-            )}
-          </>
-        ) : (
-          <Box className={cls.icon}>{children}</Box>
-        )}
-      </Fade>
+      {imageSrc ? (
+        <Fade in>
+          <img
+            alt="thumbnail"
+            draggable={false}
+            onError={handleError}
+            className={cls.img}
+            src={imageSrc}
+          />
+        </Fade>
+      ) : (
+        <Box className={cls.icon}>{children}</Box>
+      )}
     </CardMedia>
   );
 };
