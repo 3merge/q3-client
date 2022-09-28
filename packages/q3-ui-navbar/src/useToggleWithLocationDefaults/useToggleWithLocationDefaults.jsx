e38285ee99228ac9069f63e2e4ba44bb12a4136e
Mike <mibberson@3merge.ca>
2022-09-28 15:52:34 -0400

    fix: partial match on navbar links

--- packages/q3-ui-navbar/src/useToggleWithLocationDefaults/useToggleWithLocationDefaults.jsx
+++ packages/q3-ui-navbar/src/useToggleWithLocationDefaults/useToggleWithLocationDefaults.jsx
@@ -3,7 +3,13 @@ import { isObject } from 'lodash';
 import useToggleEffect from '../useToggleEffect';
 
 const useToggleWithLocationDefaults = (path) => {
-  const matches = isObject(useMatch(path));
+  const completeMatch = useMatch(path);
+  const partialMatch = useMatch(
+    // ends up being the same
+    path !== '/' ? [path, '*'].join('/') : '/',
+  );
+
+  const matches = isObject(completeMatch) || partialMatch;
 
   return {
     ...useToggleEffect(matches),
