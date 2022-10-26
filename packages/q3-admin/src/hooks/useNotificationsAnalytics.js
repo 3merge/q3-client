fed9be2fa262d471fa26ddb883fb2160c957ff85
Mike <mibberson@3merge.ca>
2022-10-26 14:05:34 -0400

    feat: notifications analytics

---
+++ packages/q3-admin/src/hooks/useNotificationsAnalytics.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import axios from 'axios';
+import { object } from 'q3-ui-helpers';
+import { join, isFunction, compact } from 'lodash';
+import { Store } from '../containers/state';
+
+const useNotificationsAnalytics = (getSubDocumentIds) => {
+  const { data } = React.useContext(Store);
+  const documentId = data?.id;
+
+  React.useLayoutEffect(() => {
+    // usually in an error or loading state
+    if (!documentId) return undefined;
+
+    const controller = new AbortController();
+    const timer = setTimeout(() => {
+      object.noop(
+        axios.post(
+          '/analytics',
+          {
+            documentId,
+            subDocumentId: isFunction(getSubDocumentIds)
+              ? join(compact(getSubDocumentIds(data)), ',')
+              : undefined,
+          },
+          {
+            signal: controller.signal,
+          },
+        ),
+      );
+    }, 500);
+
+    return () => {
+      if (timer) window.clearTimeout(timer);
+      controller.abort();
+    };
+  }, [documentId]);
+};
+
+export default useNotificationsAnalytics;
