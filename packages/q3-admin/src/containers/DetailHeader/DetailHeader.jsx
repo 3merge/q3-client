06d08b99bd9a5921c51920cf6f4c1ebad2cac8b9
Mike <mibberson@3merge.ca>
2022-11-04 09:11:58 -0400

    refactor: remove old files and reposition popover

--- packages/q3-admin/src/containers/DetailHeader/DetailHeader.jsx
+++ packages/q3-admin/src/containers/DetailHeader/DetailHeader.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { EditableTypography } from 'q3-components';
 import Typography from '@material-ui/core/Typography';
 import { useAuth } from 'q3-ui-permissions';
+import { isFunction } from 'lodash';
 import { useTitle } from '../../hooks';
 import { Dispatcher, Store, Definitions } from '../state';
 import useStyle from './styles';
@@ -12,18 +13,27 @@ const DetailHeader = (props) => {
   const { collectionName } = React.useContext(Definitions);
   const { data } = React.useContext(Store);
   const { patch } = React.useContext(Dispatcher);
-  const { canEditSub } = useAuth(collectionName);
+  const auth = useAuth(collectionName);
   const text = useTitle(data, props);
   const cls = useStyle();
 
   const { editable, titleProp: name } = props;
+  const { canEditSub } = auth;
   const typographyProps = {
     component: 'h1',
     id: 'detail-title',
     variant: 'h3',
   };
 
-  return name && canEditSub(name) && editable ? (
+  const isEditable = React.useCallback(
+    () =>
+      isFunction(editable)
+        ? editable(data, auth)
+        : editable === true,
+    [editable],
+  );
+
+  return name && canEditSub(name) && isEditable() ? (
     <Typography {...typographyProps} className={cls.h1}>
       <EditableTypography
         component="span"
